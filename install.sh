af8769a5a82cab06e60f72b1a1cc0e0dbfd25297
PlasmaHH <plasmahh@gmx.net>
2024-05-17 15:45:12 +0200

    more

--- install.sh
+++ install.sh
@@ -4,4 +4,14 @@ for i in diffs/*.diff;
 do
 	patch -p1 --dry-run < $i
 done
+
+
+for cmd in bat gdb vim git git ctags;
+do
+	x=$(which $cmd 2>/dev/null)
+	if [[ -z "$x" ]];
+	then
+		echo "Need to install ${cmd}"
+	fi
+done
 # vim: tabstop=4 shiftwidth=4 noexpandtab
