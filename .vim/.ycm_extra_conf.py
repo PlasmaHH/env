7aa67541d4acdfa780d2ea1b85fd12ba727fec75
PlasmaHH <plasmahh@gmx.net>
2025-02-04 21:58:47 +0100

    Trying to get it to work better with g++ projects

--- .vim/.ycm_extra_conf.py
+++ .vim/.ycm_extra_conf.py
@@ -4,6 +4,8 @@
 import os
 import json
 import shlex
+import copy
+import ycm_core
 
 def log( s ):
     with open("log","a+",encoding="utf-8") as l:
@@ -21,6 +23,9 @@ def win2wsl( p ):
 thispath = os.getcwd()
 ccommands = thispath + "/compile_commands.json"
 
+database = ycm_core.CompilationDatabase( thispath )
+log(f"{database=}")
+
 # TODO add automatic reloading after something in VS changed
 oldpath = ""
 while( oldpath != thispath ):
@@ -37,12 +42,15 @@ while( oldpath != thispath ):
         oldpath = thispath
         thispath = os.path.dirname(thispath)
 
+cdir = os.path.dirname(ccommands)
 #log(f"{ccommands=}")
 if( not os.path.isfile(ccommands) ):
     cdb=[]
 else:
     with open(ccommands) as cfile:
         cdb = json.load(cfile)
+
+default_flags = [ "--driver-mode=g++", "-Wall", "-Wextra","-std=gnu++2c" ]
 
 class compile_command:
 
@@ -51,12 +59,16 @@ class compile_command:
         self.file = file
         cmd = win2wsl(cmd)
         cmd = shlex.split(cmd)
-        self.flags=[ "-Wall", "-Wextra","-std=gnu++2c" ]
+        self.flags = []
+        self.flags.append( cmd[0] )
+        self.flags += copy.copy(default_flags)
         for c in cmd:
             match c:
                 case c if c.startswith("-I"):
                     self.flags.append(c)
                 case c if c.startswith("-D"):
+                    self.flags.append(c)
+                case c if c.startswith("-W"):
                     self.flags.append(c)
 #                case _:
 #                    print("c = '%s'" % (c,) )
@@ -64,6 +76,7 @@ class compile_command:
             self.flags.append("-xc")
         else:
             self.flags.append("-xc++")
+        self.flags.append(f"-c")
 
 ccdb = {}
 
@@ -76,29 +89,39 @@ for x in cdb:
     file= x["file"]
     cc = compile_command( cmd, file )
     ccdb[cc.file] = cc
+    if( not file.startswith("/") ):
+        cc = compile_command( cmd, cdir + "/" + file )
+        ccdb[cc.file] = cc
 
 #log(ccdb)
 
 def Settings( **kwargs ):
     filename = kwargs["filename"]
     cc=ccdb.get(filename,None)
+#    log(f"Settings({kwargs=})")
 #    log("filename = '%s'" % (filename,) )
-    if( cc is not None ):
-#        log(" ".join(cc.flags))
+#    compilation_info = database.GetCompilationInfoForFile( filename )
+#    log(f"{compilation_info=}")
+#    log(f"{compilation_info.compiler_flags_=}")
+#    return { "flags" : compilation_info.compiler_flags_ }
+    if( cc is not None ): # Compile command foud in DB
+        log(" ".join(cc.flags))
 #        log(str(cc))
         return { "flags" : cc.flags }
-    elif( filename.endswith(".h") ):
+
+    if( filename.endswith(".h") ): # Check if there is a cppfile with the same name and path
         cppfile = filename[:-2]
         cppfile += ".cpp"
 #        log("cppfile = '%s'" % (cppfile,) )
         cc = ccdb.get(cppfile)
         if( cc is not None ):
             return { "flags" : cc.flags }
-        elif( len(ccdb) == 0 ):
-            return { "flags" : compile_command("","").flags }
-        else:
-            cc = next(iter(ccdb.values()))
-            return { "flags" : cc.flags }
+    # No cpp file found that coresponds to the header...
+    if( len(ccdb) == 0 ): # Not a single command known...
+        return { "flags" : compile_command("","").flags } # Use a default version of the flags
+    else: # there are some, just take the first and the flags of it, possibly all are the same anyways
+        cc = next(iter(ccdb.values()))
+        return { "flags" : cc.flags }
 
     return {}
 
